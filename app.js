66cbedf4e1f9cfaa42d956599aa61a9781a23594
Tim Kent <timdekent@gmail.com>
2022-03-30 20:42:08 -0700

    finished game logic

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const playerOneButton = document.querySelector('#playerOneButton');
 const playerTwoButton = document.querySelector('#playerTwoButton');
+const resetButton = document.querySelector('#resetButton');
+const winningScoreSelect = document.querySelector('#playTo');
 
 let playerOneScore = 0;
 let playerOneDisplay = document.querySelector('#playerOneScore');
@@ -7,18 +9,52 @@ let playerOneDisplay = document.querySelector('#playerOneScore');
 let playerTwoScore = 0;
 let playerTwoDisplay = document.querySelector('#playerTwoScore');
 
-let winningScore = 5;
+let winningScore = 3;
+let isGameOver = false;
 
 playerOneButton.addEventListener('click', function() {
-    if (playerOneScore !== winningScore) {
+    if (!isGameOver) {
         playerOneScore += 1;
-        playerOneDisplay.innerText = playerOneScore;
-    }
+        if (playerOneScore === winningScore) {
+            isGameOver = true;
+            playerOneDisplay.classList.add('winner');
+            playerTwoDisplay.classList.add('loser');
+            playerOneButton.disabled = true;
+            playerTwoButton.disabled = true;
+        }
+        playerOneDisplay.innerText = playerOneScore;    
+    } 
 });
 
 playerTwoButton.addEventListener('click', function() {
-    if (playerTwoScore !== winningScore) {
+    if (!isGameOver) {
         playerTwoScore += 1;
-        playerTwoDisplay.innerText = playerTwoScore;
-    }
+        if (playerTwoScore === winningScore) {
+            isGameOver = true;
+            playerOneDisplay.classList.add('loser');
+            playerTwoDisplay.classList.add('winner');
+            playerOneButton.disabled = true;
+            playerTwoButton.disabled = true;
+        }
+        playerTwoDisplay.innerText = playerTwoScore;    
+    } 
 });
+
+winningScoreSelect.addEventListener('change', function() {
+    winningScore = parseInt(this.value);
+    reset();
+});
+
+resetButton.addEventListener('click', reset);
+
+function reset() {
+    isGameOver = false;
+    playerOneScore = 0;
+    playerTwoScore = 0;
+    playerOneDisplay.innerText = 0;
+    playerTwoDisplay.innerText = 0;
+    playerOneDisplay.classList.remove('winner', 'loser');
+    playerTwoDisplay.classList.remove('winner', 'loser');
+    playerOneButton.disabled = false;
+    playerTwoButton.disabled = false;
+}
