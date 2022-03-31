69e243965b24899c86febc42ccc71c66ca94a14d
Tim Kent <timdekent@gmail.com>
2022-03-30 19:33:26 -0700

    html, js changes

---
+++ app.js
@@ -0,0 +1,6 @@
+const playerOneButton = document.querySelector('#playerOneButton');
+const playerTwoButton = document.querySelector('#playerTwoButton');
+
+playerOneButton.addEventListener('click', function() {
+    
+})
