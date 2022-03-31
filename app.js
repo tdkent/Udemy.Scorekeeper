502075581fcb1206fa449db7875875430a09df37
Tim Kent <timdekent@gmail.com>
2022-03-30 19:50:26 -0700

    basic score logic

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const playerOneButton = document.querySelector('#playerOneButton');
 const playerTwoButton = document.querySelector('#playerTwoButton');
 
+let playerOneScore = 0;
+let playerOneDisplay = document.querySelector('#playerOneScore');
+
+let playerTwoScore = 0;
+let playerTwoDisplay = document.querySelector('#playerTwoScore');
+
+let winningScore = 5;
+
 playerOneButton.addEventListener('click', function() {
-    
-})
+    if (playerOneScore !== winningScore) {
+        playerOneScore += 1;
+        playerOneDisplay.innerText = playerOneScore;
+    }
+});
+
+playerTwoButton.addEventListener('click', function() {
+    if (playerTwoScore !== winningScore) {
+        playerTwoScore += 1;
+        playerTwoDisplay.innerText = playerTwoScore;
+    }
+});
